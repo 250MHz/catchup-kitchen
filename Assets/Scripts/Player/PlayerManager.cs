223376f31408c5679519bc72f95af1a7e94ad7cf
250MHz <250MHz@users.noreply.github.com>
2024-10-01 23:16:27 -0700

    Create prefab variants for different player models

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -5,21 +5,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private GameObject player1Prefab;
+    [SerializeField] private GameObject player2Prefab;
+    [SerializeField] private GameObject player3Prefab;
 
     // Start is called before the first frame update
     void Awake()
     {
         var player1 = PlayerInput.Instantiate(
-            playerPrefab, playerIndex: 0, controlScheme: "WASD",
+            player1Prefab, playerIndex: 0, controlScheme: "WASD",
             pairWithDevice: Keyboard.current
         );
         var player2 = PlayerInput.Instantiate(
-            playerPrefab, playerIndex: 1, controlScheme: "IJKL",
+            player2Prefab, playerIndex: 1, controlScheme: "IJKL",
             pairWithDevice: Keyboard.current
         );
         var player3 = PlayerInput.Instantiate(
-            playerPrefab, playerIndex: 2, controlScheme: "Arrows",
+            player3Prefab, playerIndex: 2, controlScheme: "Arrows",
             pairWithDevice: Keyboard.current
         );
         player1.transform.parent.transform.position = new Vector3(0, 0, -3);

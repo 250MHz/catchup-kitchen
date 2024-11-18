0379ca9c8d80415ccf602224e48fe5e979e7e5de
250MHz <250MHz@users.noreply.github.com>
2024-11-18 02:35:02 -0800

    Add more background cosmetics
    Added static vehicles. Added 2 animated dogs. Added 1 animated
    character.

---
+++ Assets/Scripts/Extra/DogAnimator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogAnimator : MonoBehaviour
+{
+    [SerializeField] private bool isEating;
+    [SerializeField] private bool isSitting;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        if (isEating)
+        {
+            animator.SetFloat("Speed_f", 0f);
+            animator.SetBool("Eat_b", isEating);
+        } else if (isSitting) {
+            animator.SetBool("Sit_b", isSitting);
+        }
+    }
+}
